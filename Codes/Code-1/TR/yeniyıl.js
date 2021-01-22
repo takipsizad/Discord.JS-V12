01b351aadae5ce4c9b66f096c01c3fd0467bf6ed
takipsizad <addeniz25@gmail.com>
2021-01-22 11:47:03 +0300

    eeeeeee

--- Codes/Code-1/TR/yeniyıl.js
+++ Codes/Code-1/TR/yeniyıl.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const parsems = require('parse-ms');
 
 exports.run = async(client, message, args) => {
-  let yeniyil = new Date('2021-01-01:00:00')
+  let yeniyil = new Date('2022-01-01:00:00')
     let zaman = parsems(yeniyil - Date.now())
     message.reply(`Yeni yıla **${zaman.days}** gün,**${zaman.hours}** saat,**${zaman.minutes}**dakika sonra gireceğiz :tada:`)
 }
